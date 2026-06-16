482eda7c7b2a4af4fd825857feea4b8c5e9efbfe
AngeiSSI <anelroro@outlook.es>
2026-06-15 19:18:42 -0500

    Modificaciones

--- frontend/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://api-infra-actividades-gwevagd8g0dfaxcv.westcentralus-01.azurewebsites.net'
-};
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { appRoutes } from './app.routes';
-import { AuthInterceptorClass } from './interceptors/auth.interceptor';
+import { routes } from './app.routes';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(appRoutes),
+    provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
     {
       provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorClass,
+      useClass: AuthInterceptor,
       multi: true
     }
   ]
